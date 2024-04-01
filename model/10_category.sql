5a31c8dce7257d02f607c3cead5c0e033f189ef1
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-04-01 22:37:38 +0200

    Updated app

--- model/10_category.sql
+++ model/10_category.sql
@@ -46,8 +46,12 @@ begin
 end category_trg_01;
 /
 
-insert into category (type, color, name, icon) values ('E', 'u-color-1', 'Car maintenance', 'fa-car');
-insert into category (type, color, name, icon) values ('E', 'u-color-7', 'Car payment',     'fa-car');
+insert into category (type, color, name, icon) values ('E', 'u-color-9', 'Car maintenance', 'fa-car');
+insert into category (type, color, name, icon) values ('E', 'u-color-9', 'Car payment',     'fa-car');
+insert into category (type, color, name, icon) values ('E', 'u-color-9', 'Childcare',       'fa-child');
+
+insert into category (type, color, name, icon) values ('I', 'u-color-4', 'Job',             'fa-suitcase');
+insert into category (type, color, name, icon) values ('I', 'u-color-4', 'Consultancy',     'fa-flask');
 
 commit;
 
